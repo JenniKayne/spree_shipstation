d48752658c3021a79134d1849dda3fd945596a25
Paweł Strzałkowski <pawel@praesens.co>
2018-02-28 22:25:09 +0100

    E giftcard flow

--- app/models/spree/shipment/shipstation_export.rb
+++ app/models/spree/shipment/shipstation_export.rb
@@ -2,7 +2,7 @@ module Spree
   class Shipment < Spree::Base
     module ShipstationExport
       def shipstation_valid?
-        inventory_units.all? { |unit| unit.variant.shipstation_valid? }
+        inventory_units.any? { |unit| unit.variant.shipstation_valid? }
       end
     end
   end
